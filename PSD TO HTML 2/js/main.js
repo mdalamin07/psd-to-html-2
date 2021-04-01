25e8ab7894d84abbc45b6fe9114bc1f6f95b01aa
mdalamin07 <66849857+mdalamin07@users.noreply.github.com>
2021-04-01 06:17:21 +0600

    Upload Files

---
+++ PSD TO HTML 2/js/main.js
@@ -0,0 +1,10 @@
+// Bx Slider
+  $(document).ready(function(){
+    $('.slider').bxSlider();
+    $('#main_menu').meanmenu({
+      meanMenuContainer: '#mobile_menu',
+      meanScreenWidth: "991",
+    });
+  });
+
+
